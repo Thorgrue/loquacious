2f557fa498705e8a6c0562de8378e458e34c5250
Gagou <gaelle.shearn.jmr@gmail.com>
2021-02-19 11:27:31 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       'title-play': ['"Playfair Display SC"', 'serif'],
+       'body-philo': ['Philosopher', 'sans-serif']
+      },
+    },
   },
   variants: {
     extend: {},
