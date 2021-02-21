054ec2ae2c3dee7ee8697bffa09988522e9d6ea6
Gagou <gaelle.shearn.jmr@gmail.com>
2021-02-21 22:57:54 +0100

    new background + 10 seed good + align word & cat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ puts 'Cleaning database...'
 puts 'Creating words...'
 
   urls = [
+        "https://cdn.pixabay.com/photo/2016/08/18/14/25/watercolour-texture-1603104_1280.jpg",
         "https://cdn.pixabay.com/photo/2017/08/10/02/05/tiles-shapes-2617112_1280.jpg",
         "https://s8v8k3v9.stackpathcdn.com/wp-content/uploads/2020/07/Set-Of-Watercolor-Eucalyptus-Leaves-9.jpg",
-        "https://cdn.pixabay.com/photo/2018/06/22/11/57/lavender-3490599_1280.jpg",
+        "https://cdn.pixabay.com/photo/2016/04/08/19/51/watercolor-1316867_1280.jpg",
         "https://s8v8k3v9.stackpathcdn.com/wp-content/uploads/2020/09/Risograph-for-Procreate-3-scaled.jpg",
-        "https://thumbs.dreamstime.com/z/seamless-pattern-ginkgo-biloba-leaves-linear-drawing-watercolor-spots-can-be-used-wallpaper-fills-background-textile-web-177004887.jpg",
         "https://s8v8k3v9.stackpathcdn.com/wp-content/uploads/2021/02/Alcohol-Ink-Texture-14-3.jpg",
         "https://cutewallpaper.org/21/tumblr-background-watercolor/Watercolor-Pattern-at-GetDrawings.com-Free-for-personal-.jpg",
         "https://wallpaperforu.com/wp-content/uploads/2020/07/yellow-aesthetic-wallpaper-20072002141919.jpg",
@@ -17,54 +17,56 @@ puts 'Creating words...'
         "https://eskipaper.com/images/free-pattern-backgrounds-1.jpg",
           ]
 
-  callipyge = Word.create(name: 'Callipyge', cat: 'adj.', day: '2021-02-22',
+puts 'Callipyge, gadru, vespéral, quintessence, mirliflore...'
+  callipyge = Word.create(name: 'Callipyge', cat: 'adj.', day: '2021-03-01',
                           origin: "Adjectif faisant allusion à un type de statues antiques de Vénus Callipyge : elles représentent Vénus soulevant son péplos pour se mirer dans l'eau et regarder ses fesses, nécessairement superbes par-dessus l'épaule.",
                           definition: "De nos jours, Callipyge est utilisé pour parler d'une personne qui a de belles fesses, harmonieusement arrondies.",
                           url: urls[0])
 
-  gadru = Word.create(name: 'Gadru', cat: 'adj.', day: '2021-02-23',
+  gadru = Word.create(name: 'Gadru·e', cat: 'adj.', day: '2021-03-02',
                       origin: "Cet adjectif picard, qui à l’origine décrivait un petit enfant en bonne santé, a évolué dans le temps. On dit d’un tout jeune enfant qui est vif, éveillé, gaillard, bien portant, qu’il est bien gadru. ",
                       definition: "Gadru est maintenant utilisé comme un substantif pour décrire des gens qui sont en pleine forme ou bien des bons vivants ; quelque chose de fort et/ou avec une bonne vitalité.",
                       url: urls[1])
 
-  vespéral = Word.create(name: 'Vespéral', cat: 'adj.', day: '2021-02-24',
+  vespéral = Word.create(name: 'Vespéral·e', cat: 'adj.', day: '2021-03-03',
                         origin: 'Vespéral pouvait désigner, dans la lithurgie catholique, un livre contenant les prières du soir, les vêpres et complies.',
                         definition: 'Plus couramment : qui se déroule, qui a lieu le soir. Une clarté vespérale, des rayons vespéraux.',
                         url: urls[2])
 
-  callip = Word.create(name: 'Callip', cat: 'adj.', day: '2021-02-25',
-                      origin: "Adjectif faisant allusion à un type de statues antiques de Vénus Callipyge : elles représentent Vénus soulevant son péplos pour se mirer dans l'eau et regarder ses fesses, nécessairement superbes par-dessus l'épaule.",
-                      definition: "De nos jours, Callipyge est utilisé pour parler d'une personne qui a de belles fesses, harmonieusement arrondies.",
+  quintessence = Word.create(name: 'Quintessence', cat: 'n.f.', day: '2021-03-04',
+                      origin: "Utilisé pour nommer de l'extrait le plus concentré ou le plus délicat. Utilisé aussi pour nommer la partie la plus subtile d'une substance.",
+                      definition: "Généralement, quintessence désigne ce qu'il y a de meilleur et de plus raffiné dans quelque chose.",
                       url: urls[3])
 
-  gadr = Word.create(name: 'Gadr', cat: 'adj.', day: '2021-02-26',
-                    origin: "Cet adjectif picard, qui à l’origine décrivait un petit enfant en bonne santé, a évolué dans le temps. On dit d’un tout jeune enfant qui est vif, éveillé, gaillard, bien portant, qu’il est bien gadru. ",
-                    definition: "Gadru est maintenant utilisé comme un substantif pour décrire des gens qui sont en pleine forme ou bien des bons vivants ; quelque chose de fort et/ou avec une bonne vitalité.",
+  mirliflore = Word.create(name: 'Mirliflore', cat: 'n.m.', day: '2021-03-05',
+                    origin: "Un mirliflore est un jeune homme très porté sur son élégance et sur son apparence, \"mirliflor\" comme les mille fleurs de son parfum.",
+                    definition: "Aujourd'hui, ce terme un peu vieilli décrit un jeune homme agissant de façon fière voire hautaine.",
                     url: urls[4])
 
-  vespér = Word.create(name: 'Vespér', cat: 'adj.', day: '2021-02-27',
-                      origin: 'Vespéral pouvait désigner, dans la lithurgie catholique, un livre contenant les prières du soir, les vêpres et complies.',
-                      definition: 'Plus couramment : qui se déroule, qui a lieu le soir. Une clarté vespérale, des rayons vespéraux.',
+puts 'Lampadophore, haptique, pabullophobie, lénifier, faribole ...'
+  lampadophore = Word.create(name: 'Lampadophore', cat: 'adj.', day: '2021-03-06',
+                      origin: 'Rôle qui peut être décisif dans certaines situation, la personne lampadophore pouvait donnait le signal de départ d\'une course ou d\'un sport.',
+                      definition: "De façon plus globale, lampadophore désigne être porteur·se de flambeau.",
                       url: urls[5])
 
-  vesp = Word.create(name: 'Vesp', cat: 'adj.', day: '2021-02-28',
-                      origin: 'Vespéral pouvait désigner, dans la lithurgie catholique, un livre contenant les prières du soir, les vêpres et complies.',
-                      definition: 'Plus couramment : qui se déroule, qui a lieu le soir. Une clarté vespérale, des rayons vespéraux.',
+  haptique = Word.create(name: 'Haptique', cat: 'n.f.', day: '2021-03-07',
+                      origin: "L'haptique est une science comme l'optique ou l'acoustique, mais elle décrit la science liée à la sensation du toucher.",
+                      definition: "Haptique évoque surtout le ressenti produit au niveau du cerveau lors d'un contact avec sa peau.",
                       url: urls[6])
 
-  ves = Word.create(name: 'Ves', cat: 'adj.', day: '2021-03-01',
-                    origin: 'Vespéral pouvait désigner, dans la lithurgie catholique, un livre contenant les prières du soir, les vêpres et complies.',
-                    definition: 'Plus couramment : qui se déroule, qui a lieu le soir. Une clarté vespérale, des rayons vespéraux.',
+  pabullophobie = Word.create(name: 'Pabullophobie', cat: 'n.f', day: '2021-03-08',
+                    origin: "Comme tout mot finissant par -phobie, la Pabullophobie est une peur.",
+                    definition: "En l'occurence, c'est la peur des brouettes.",
                     url: urls[7])
 
-  gad = Word.create(name: 'Gad', cat: 'adj.', day: '2021-03-02',
-                    origin: "Cet adjectif picard, qui à l’origine décrivait un petit enfant en bonne santé, a évolué dans le temps. On dit d’un tout jeune enfant qui est vif, éveillé, gaillard, bien portant, qu’il est bien gadru. ",
-                    definition: "Gadru est maintenant utilisé comme un substantif pour décrire des gens qui sont en pleine forme ou bien des bons vivants ; quelque chose de fort et/ou avec une bonne vitalité.",
+  lénifier = Word.create(name: 'Lénifier', cat: 'vb.', day: '2021-03-09',
+                    origin: "Ce verbe transitif vient du \"lénitif\" un médicament utilisé pour apaiser quelqu'un. ",
+                    definition: "Maintenant lénifier est utilisé pour signifier calmer, adoucir, apaiser une peine. Attention, lénifier peut également signifier amollir quelqu'un.",
                     url: urls[8])
 
-  call = Word.create(name: 'Call', cat: 'adj.', day: '2021-03-03',
-                    origin: "Adjectif faisant allusion à un type de statues antiques de Vénus Callipyge : elles représentent Vénus soulevant son péplos pour se mirer dans l'eau et regarder ses fesses, nécessairement superbes par-dessus l'épaule.",
-                    definition: "De nos jours, Callipyge est utilisé pour parler d'une personne qui a de belles fesses, harmonieusement arrondies.",
+  faribole = Word.create(name: 'Faribole', cat: 'n.f.', day: '2021-03-10',
+                    origin: "Une faribole est une phrase légère, peu sérieuse. Une faribole n'a que peu de valeur, elle est frivole et peu être vaine de sens.",
+                    definition: "La faribole est un propos peu important et sans but particulier, elle reste néanmoins agréable.",
                     url: urls[9])
 
   # new = Word.create(name: '', cat: '', day: '',
