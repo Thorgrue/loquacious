3a71f4698b4b6ccb3f3646cb705b87f73bf1c268
Gagou <gaelle.shearn.jmr@gmail.com>
2021-03-02 16:57:42 +0100

    search bar sur la home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    if params[:query].present?
+      sql_query = " \
+        words.name ILIKE :query \
+        OR words.definition ILIKE :query \
+        OR words.example ILIKE :query \
+      "
+      @words = Word.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @words = []
+    end
   end
 end
