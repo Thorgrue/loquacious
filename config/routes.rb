1afd14936dd98ff1c0e98040e7e8d7f39124996a
Gaëlle <gaelle.shearn.jmr@gmail.com>
2021-06-04 17:27:41 +0200

    custom url with date

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :words, only: [:index, :show] do
+
+get 'words/:day', to: 'words#show', as: 'word'
+  resources :words, only: [:index] do
     collection do
       get 'calendar'
     end
   end
+
 
   get 'error', to: 'words#error'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
