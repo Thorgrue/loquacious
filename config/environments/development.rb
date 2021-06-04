67df780b74a6d93fda89c43476a86c6cf0ef015f
Gaëlle <gaelle.shearn.jmr@gmail.com>
2021-06-04 16:13:35 +0200

    month in French

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+I18n.enforce_available_locales = false
+I18n.config.available_locales = :fr
+config.i18n.default_locale = :fr
 end
