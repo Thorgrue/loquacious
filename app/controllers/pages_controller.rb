b412cd296df66b8bb90b3b8121cd048005d752e1
Gaëlle <gaelle.shearn.jmr@gmail.com>
2021-06-13 20:04:41 +0200

    v2 feature visible letters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,9 @@
 class PagesController < ApplicationController
-  before_action :set_today, only: [:home]
+  before_action :set_today, only: [:home, :set_visible_letters]
+  before_action :set_visible_letters, only: [:home]
 
   def home
-    @word = @today
-    @teasing = ""
-    (@word.name.length - 1).times do
-      @teasing = @teasing + '_'
-    end
+
 
     if params[:query].present?
       sql_query = " \
@@ -23,7 +20,18 @@ class PagesController < ApplicationController
   private
 
   def set_today
-    @now = Date.today
-    @today = Word.find_by(day: @now)
+    now = Date.today
+    @today = Word.find_by(day: now)
+  end
+
+  def set_visible_letters
+    @nb_max_letters = (@today.name.length - 1)*2/3
+    @interval = 12.0/@nb_max_letters
+    @beginning_of_this_hour = Time.now.beginning_of_hour.to_s(:time).first(2).to_i
+    if @beginning_of_this_hour > 8
+      @nb_now_letters = ((@beginning_of_this_hour - 8)/@interval).round
+    else
+      @nb_now_letters = 1
+    end
   end
 end
